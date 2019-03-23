c3b1f039a40fca37c9bfec2df67be07bf729eefd
brain-1 <j.brain@gmail.com>
2019-03-23 20:44:45 +0300

    make reccurent function

--- c/poem.c
+++ c/poem.c
@@ -1,10 +1,10 @@
 #include "stdio.h"
-const char* couplet = "у попа была собака, он ее любил,\nона съела кусок мяса";
+const char* couplet = "у попа была собака, он ее любил,\nона съела кусок мяса, он ее убил\nв землю закопал\nи надпись написал, что ";
 
 void poem(){
     printf ("%s",couplet);
+    poem();
 }
-
 
 void main(){
     poem();

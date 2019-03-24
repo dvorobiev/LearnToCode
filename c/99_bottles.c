a6de45e6cbbe34e633863942e7a215bb68987600
brain-1 <j.brain@gmail.com>
2019-03-24 23:31:57 +0300

    optimization function couplet()

--- c/99_bottles.c
+++ c/99_bottles.c
@@ -1,34 +1,51 @@
-#include "stdio.h"
-
 // 99 bottles of beer on the wall, 99 bottles of beer.
 // Take one down, pass it around, 98 bottles of beer on the wall...
+//
 // 98 bottles of beer on the wall, 98 bottles of beer.
 // Take one down, pass it around, 97 bottles of beer on the wall...
+//
 // 97 bottles of beer on the wall, 97 bottles of beer.
 // Take one down, pass it around, 96 bottles of beer on the wall...
 // ...
+// 2 bottles of beer on the wall, 2 bottles of beer.
+// Take one down, pass it around, 1 bottle of beer on the wall...
+// 
+// 1 bottle of beer on the wall, 1 bottle of beer.
+// Take one down, pass it around, no more bottles of beer....
+//
 // No more bottles of beer on the wall, no more bottles of beer.
 // We've taken them down and passed them around; now we're drunk and passed out!
-int bottle_count = 99;
 
-void couplet(int bottle_count){
-    printf("%i bottles of beer on the wall, %i bottles of beer.\n", bottle_count, bottle_count);
-    printf("Take one down, pass it around,");
-    --bottle_count;
-    if (bottle_count>0){
-        printf(" %i bottles of beer on the wall...\n\n", bottle_count);
+#include "stdio.h"
+
+int count = 99; //bottles count
+
+void couplet(int count){
+    if (count>1){
+        printf("%i bottles of beer on the wall, %i bottles of beer.\n", count, count);
+        printf("Take one down, pass it around,");
+        --count;
+        if (count!=1){
+            printf(" %i bottles of beer on the wall...\n\n", count);
+        }
+        else{
+            printf(" %i bottle of beer on the wall...\n\n", count);
+        }
+        couplet(count);
     }
-    else{
-        printf(" no more bottles of beer on the wall...\n\n");
+    else if (count==1){
+        printf("%i bottle of beer on the wall, %i bottle of beer.\n", count, count);
+        printf("Take one down, pass it around, no more bottles of beer on wall....\n\n");
+        --count;
+        couplet(count);
     }
-    if (bottle_count==0){
-        printf("No more bottles of beer on the wall, no more bottles of beer.\nWe've taken them down and passed them around; now we're drunk and passed out!");
-    } else{
-        couplet(bottle_count);
+    else
+    {
+        printf("No more bottles of beer on the wall, no more bottles of beer.\n");
+        printf("We've taken them down and passed them around; now we're drunk and passed out!");
     }
 }
 
 int main(){
-    printf("===Right now we print POEM of \"99 bottles of beer on the wall\"===\n\n");
-    couplet(bottle_count);
+    couplet(count);
 }

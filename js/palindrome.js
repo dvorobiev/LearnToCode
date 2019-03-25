a7796ced969d425e3221dbd058c9c1656671eb69
brain-1 <j.brain@gmail.com>
2019-03-25 16:02:27 +0300

    add "else"

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,21 +1,24 @@
-punct = ". ,:'"
-function is_palindrom(palindrome){
-    if (palindrome.length<2){
+punct = "., ':"
+function is_palindrom(palindrome) {
+    if (palindrome.length < 2) {
         console.log("True");
-        return true;
+        return 1;
     } else {
-            if (punct.indexOf(palindrome[0])!=-1){
-                is_palindrom(palindrome.substr(1,palindrome.length-1));
+        if (punct.indexOf(palindrome[0]) != -1) {
+            is_palindrom(palindrome.substr(1, palindrome.length - 1));
+        } else {
+            if (punct.indexOf(palindrome[palindrome.length - 1]) != -1) {
+                is_palindrom(palindrome.substr(0, palindrome.length - 1));
             }
-            if (punct.indexOf(palindrome[palindrome.length-1])!=-1){
-                is_palindrom(palindrome.substr(0,palindrome.length-1));
+            else {
+                if (palindrome[0].toLowerCase() == palindrome[palindrome.length - 1].toLowerCase()) {
+                    is_palindrom(palindrome.substr(1, palindrome.length - 2));
+                } else {
+                    console.log("False");
+                    return 2;
+                }
             }
-            if(palindrome[0].toLowerCase()==palindrome[palindrome.length-1].toLowerCase()) {
-                is_palindrom(palindrome.substr(1,palindrome.length-2));
-            } else {
-                console.log("False");
-                return false;
-            }
+        }
     }
 }
 

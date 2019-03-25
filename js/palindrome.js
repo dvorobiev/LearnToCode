c84f931178e79d61402208f6c48de933b18cfbb0
brain-1 <j.brain@gmail.com>
2019-03-25 16:14:00 +0300

    corect syntax (else if)

--- js/palindrome.js
+++ js/palindrome.js
@@ -3,22 +3,15 @@ function is_palindrom(palindrome) {
     if (palindrome.length < 2) {
         console.log("True");
         return 1;
+    } else if (punct.indexOf(palindrome[0]) != -1) {
+        is_palindrom(palindrome.substr(1, palindrome.length - 1));
+    } else if (punct.indexOf(palindrome[palindrome.length - 1]) != -1) {
+        is_palindrom(palindrome.substr(0, palindrome.length - 1));
+    } else if (palindrome[0].toLowerCase() == palindrome[palindrome.length - 1].toLowerCase()) {
+        is_palindrom(palindrome.substr(1, palindrome.length - 2));
     } else {
-        if (punct.indexOf(palindrome[0]) != -1) {
-            is_palindrom(palindrome.substr(1, palindrome.length - 1));
-        } else {
-            if (punct.indexOf(palindrome[palindrome.length - 1]) != -1) {
-                is_palindrom(palindrome.substr(0, palindrome.length - 1));
-            }
-            else {
-                if (palindrome[0].toLowerCase() == palindrome[palindrome.length - 1].toLowerCase()) {
-                    is_palindrom(palindrome.substr(1, palindrome.length - 2));
-                } else {
-                    console.log("False");
-                    return 2;
-                }
-            }
-        }
+        console.log("False");
+        return 2;
     }
 }
 

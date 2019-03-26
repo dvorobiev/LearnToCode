d58e2502edeb80052dcdac83a07f0c19d4726b55
brain-1 <j.brain@gmail.com>
2019-03-26 15:24:14 +0300

    remove 'first"

--- js/parentheses_validation.js
+++ js/parentheses_validation.js
@@ -1,13 +1,9 @@
 //Напишите-ка, например, рекурсивную функцию, которая проверяет правильность расстановки скобок:
-//
 //()()) — неправильно
 //)( — неправильно
 //(()(()())) — правильно (вроде). Ну вы поняли. 
-//
 //Бонус пойнтс: допускать (и игнорировать) любые произвольные символы внутри скобок
-//
 //Это задача со звёздочкой, необязательная :)
-let first = true
 
 function parentheses_validation(string) {
     if (string.length==0) {
@@ -22,7 +18,7 @@ function parentheses_validation(string) {
     if (string[0]!="(" && string[0]!=")"){
         parentheses_validation(string.substr(1,string.length-1))
     } else if (string[0]==')'){
-        if ((first) || (counter==0)) {
+        if (counter==0) {
             console.log("no valid")
             return false; 
         }
@@ -42,4 +38,3 @@ console.log(parentheses_validation('(2*2)%(5+7)'))
 console.log(parentheses_validation('()()'))
 console.log(parentheses_validation(')('))
 console.log(parentheses_validation('(()(()())'))
-

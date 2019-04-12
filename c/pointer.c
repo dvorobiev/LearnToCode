8feb9bcddf7cebab57e6c5b63a73ed493e661b65
dvorobiev <j.brain@gmail.com>
2019-04-12 17:47:08 +0300

    add function reversive comporator

--- c/pointer.c
+++ c/pointer.c
@@ -1,5 +1,22 @@
 #include "stdio.h"
 #include "stdlib.h"
+int array[10]={3,5,1,7,2,7,6,0,8,4};
+
+int reverse_comp(const void *a, const void *b){
+    int* aa = (int*)a;
+    int* bb = (int*)b;
+    if (*aa<*bb){
+        return 1;
+    } else if (*aa>*bb)
+    {
+        return -1;
+    } else
+    {
+        return 0;
+    }
+    
+    
+}
 
 void multiplication(int *x){
     *x = *x*2;
@@ -7,7 +24,10 @@ void multiplication(int *x){
 
 int main(){
     int x = 42;
+    int y = 12;
     printf("%d\n", x);
     multiplication(&x);
     printf("%d\n", x);
+    printf("%d\n",reverse_comp(&x,&y));
+    qsort(&array,10,sizeof(int),&reverse_comp);
 }
